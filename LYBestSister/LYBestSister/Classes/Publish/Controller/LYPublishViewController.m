2cdd7db285b0fa26e9ef7dc3d7174d816f128f66
zhanghaibin <zhanghaibin@zhanghaibindeMacBook-Pro.local>
2016-05-14 18:47:04 +0800

    【发布模块】弹出动画

--- LYBestSister/LYBestSister/Classes/Publish/Controller/LYPublishViewController.m
+++ LYBestSister/LYBestSister/Classes/Publish/Controller/LYPublishViewController.m
@@ -8,28 +8,52 @@
 
 #import "LYPublishViewController.h"
 #import "LYPublishButton.h"
+#import <POP.h>
 
 @interface LYPublishViewController ()
 
 /** 标语 */
 @property (nonatomic, weak) UIImageView *sloganView;
 
+/** 动画时间 */
+@property (nonatomic, strong) NSArray *times;
+
 @end
 
 @implementation LYPublishViewController
+
+#pragma mark - 懒加载
+- (NSArray *)times
+{
+    if (!_times) {
+        CGFloat interval = 0.1; // 时间间隔
+        _times = @[@(5 * interval),
+                   @(4 * interval),
+                   @(3 * interval),
+                   @(2 * interval),
+                   @(0 * interval),
+                   @(1 * interval),
+                   @(6 * interval)]; // 标语的动画时间
+    }
+    return _times;
+}
 
 
 #pragma mark - 初始化
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // 添加标语
-    UIImageView *sloganView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"app_slogan"]];
-    sloganView.y = LYScreenH * 0.15;
-    sloganView.centerX = LYScreenW * 0.5;
-    [self.view addSubview:sloganView];
-    self.sloganView = sloganView;
+    // 设置标语
+    [self setupSloganView];
     
+    // 设置button
+    [self setupButton];
+    
+    
+}
+
+#pragma mark -- 设置button
+- (void)setupButton {
     // 按钮数据
     NSArray *images = @[@"publish-video", @"publish-picture", @"publish-text", @"publish-audio", @"publish-review", @"publish-offline"];
     NSArray *titles = @[@"发视频", @"发图片", @"发段子", @"发声音", @"审帖", @"离线下载"];
@@ -46,19 +70,54 @@
     for (int i = 0; i < count; i++) {
         // 创建、添加
         LYPublishButton *button = [LYPublishButton buttonWithType:UIButtonTypeCustom];
+        button.width = -1; // 按钮的尺寸为0，还是能看见文字缩成一个点，设置按钮的尺寸为负数，那么就看不见文字了
         [button addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchUpInside];
         [self.view addSubview:button];
         
-        // frame
-        CGFloat buttonX = (i % maxColsCount) * buttonW;
-        CGFloat buttonY = buttonStartY + (i / maxColsCount) * buttonH;
-        button.frame = CGRectMake(buttonX, buttonY, buttonW, buttonH);
         
         // 内容
         [button setImage:[UIImage imageNamed:images[i]] forState:UIControlStateNormal];
         [button setTitle:titles[i] forState:UIControlStateNormal];
+        
+        // frame
+        CGFloat buttonX = (i % maxColsCount) * buttonW;
+        CGFloat buttonY = buttonStartY + (i / maxColsCount) * buttonH;
+        
+        // 动画
+        POPSpringAnimation *anim = [POPSpringAnimation animationWithPropertyNamed:kPOPViewFrame];
+        anim.fromValue = [NSValue valueWithCGRect:CGRectMake(buttonX, buttonY - LYScreenH, buttonW, buttonH)];
+        anim.toValue = [NSValue valueWithCGRect:CGRectMake(buttonX, buttonY, buttonW, buttonH)];
+        anim.springSpeed = 10;
+        anim.springBounciness = 10;
+        // CACurrentMediaTime()获得的是当前时间
+        anim.beginTime = CACurrentMediaTime() + [self.times[i] doubleValue];
+        [button pop_addAnimation:anim forKey:nil];
+
     }
+
+}
+
+#pragma mark -- 设置标语
+- (void)setupSloganView {
     
+    CGFloat sloganY = LYScreenH * 0.2;
+    
+    // 添加标语
+    UIImageView *sloganView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"app_slogan"]];
+    sloganView.y = sloganY - LYScreenH;
+    sloganView.centerX = LYScreenW * 0.5;
+    [self.view addSubview:sloganView];
+    self.sloganView = sloganView;
+    
+    // 动画
+    POPSpringAnimation *anim = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
+    anim.toValue = @(sloganY);
+    anim.springSpeed = 10;
+    anim.springBounciness = 10;
+    // CACurrentMediaTime()获得的是当前时间
+    anim.beginTime = CACurrentMediaTime() + [self.times.lastObject doubleValue];
+    [sloganView.layer pop_addAnimation:anim forKey:nil];
+
 }
 
 - (void)buttonClick:(LYPublishButton *)button {
